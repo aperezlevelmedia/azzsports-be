96a06c19afeca3dc2bd460cce384e35205bebd49
Ariel Perez <ariel.perez.dev@gmail.com>
2021-01-28 19:18:13 -0400

    Change odd source to Betslipapi

--- app/Http/Controllers/JsonController.php
+++ app/Http/Controllers/JsonController.php
@@ -21,11 +21,14 @@ use Illuminate\Filesystem\Filesystem;
  */
 class JsonController extends ApiController{
     public function league(Request $request)
-    {
-        $league_url = env('LEAGUE');
-        $response = Http::get($league_url.$request->sport_id)->body();
-        $league = json_decode($response,True);
-        return response()->json(['success'=>true, 'data'=>$league, 'status_code' => 200, 'state' => true], 200);
+    {       
+        $odd_directory = env('ODDS_DIRECTORY');
+        //$path = glob(storage_path() .$odd_directory.$league.".json");
+        $path = storage_path() .$odd_directory."LEAGUES/leagues.json";            
+            
+        $file = File::get($path);
+        $leagueArray = json_decode($file,TRUE);
+        return response()->json(['success'=>true, 'data'=>$leagueArray, 'status_code' => 200, 'state' => true], 200); 
     }
     public function leagueTable(Request $request)
     {   
@@ -39,70 +42,83 @@ class JsonController extends ApiController{
     {
         $league = $request->name;
         $odd_directory = env('ODDS_DIRECTORY');
-        $path = glob(storage_path() .$odd_directory.$league."*.json");
-        
-        foreach ($path as $singleFile)
-        {
+        //$path = glob(storage_path() .$odd_directory.$league.".json");
+        $path = storage_path() .$odd_directory.$league.".json";            
             
-            $file = File::get($singleFile);
-            $leagueArray = json_decode($file,TRUE);
-            return response()->json(['success'=>true, 'data'=>$leagueArray, 'status_code' => 200, 'state' => true], 200);
-        }
+        $file = File::get($path);
+        $leagueArray = json_decode($file,TRUE);
+        return response()->json(['success'=>true, 'data'=>$leagueArray, 'status_code' => 200, 'state' => true], 200);        
 
         
     }
     public function store(Request $request)
     {
-        $oddsUrl = env('ODDS_XML');
+        $oddsUrl = env('BETSLIPAPI');
+        $leagueListUrl = env('BETSLIPAPI_LEAGUES');
         $odd_directory = env('ODDS_DIRECTORY');
-        $response = Http::get($oddsUrl)->body();
         
-        $xml = simplexml_load_string($response);
-        $ugly_json = json_decode(json_encode($xml));
-        $nice_json = $this->cleanupJson($ugly_json);
-        $newJson = json_encode($nice_json, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
-        $array = json_decode($newJson,TRUE);
-        $finalArray = null;
+        $leaguesListBySport = Http::withToken('MTp0ZXN0MQ==')->get($leagueListUrl)->json();        
+        $finalLeagueList = array();
         
-        $currentFiles = Storage::disk('public')->allFiles('ODDS/');
-        
-        foreach($currentFiles as $currentFile)
+        foreach($leaguesListBySport['Sport'] as $leagueList)
         {
-            Storage::disk('public')->delete($currentFile);
-        }       
-               
-        foreach ($array as $sportKey=>$sport) {
-            if($sportKey == 'HLW_Sport')
-            {
-                foreach ($sport as $subSportKey=>$subSport) {                                                           
-
-                    foreach ($subSport as $leagueKey => $league)
-                    {
-                        if($leagueKey == 'HLW_SubSport')
-                        {
-                            $fileName = '';
-                            if(array_key_exists('id', $league))
-                            {
-                                Storage::disk('public')->put('ODDS/'.str_replace(array(' ','/'), '', $league['name']).date('Ymdhs', time()).'.json', json_encode($league));
-                            }
-                            else{
-                                foreach ($league as $unitKey => $item)
-                                {
-                                    Storage::disk('public')->put('ODDS/'.str_replace(array(' ','/'), '', $item['name']).date('Ymdhs', time()).'.json', json_encode($item));
-                                }
-                            }
-
-                            $finalArray = $league;
-                            
-                        }
-                    }
-
-                }
+            $cont = 0;
+            foreach($leagueList['Leagues'] as $league)
+            {                        
+                $finalLeagueList[] = [ 'id' => $league['IdLeague'], 'name' => $league['Description'], 'short_name' => $this->trueFileName($league['Description']) ];                
+                $cont++;
             }
         }
         
+        Storage::disk('public')->put('ODDS/LEAGUES/'.'leagues.json', json_encode($finalLeagueList));
+        
+        foreach($finalLeagueList as $singleLeague)
+        {   $enabledLeagues = [389,784,272,3,564,7];
+            if(in_array($singleLeague['id'], $enabledLeagues))
+            {
+                $response = Http::post($oddsUrl, [
+                'Gmt' => 4,
+                'NextHour' => false,
+                'SiteId' => 1,
+                'IdPlayer' => 0,
+                'Player' => "test10",
+                'LineStyle' => "D",
+                'LeagueList' => [
+                    $singleLeague['id']
+                ]
+                ])->json();
+                $trueFileName = $this->trueFileName($singleLeague['name']);
+                Storage::disk('public')->put('ODDS/'.$trueFileName.'.json', json_encode($response));
+            }
+        }       
 
         return response()->json(['success'=>'Json files created!', 'message'=>'test', 'status_code' => 200, 'state' => 'test'], 200);
+    }
+    
+    public function trueFileName($fileName)
+    {
+        $convertedName = "";
+        switch ($fileName){
+            case 'MLB DIVISION ODDS':
+                $convertedName = 'MLB';
+                break;
+            case 'NATIONAL HOCKEY LEAGUE':
+                $convertedName = 'NHL';
+                break;
+            case 'NBA':
+                $convertedName = 'NBA';
+                break;
+            case 'NCAA BASKETBALL':
+                $convertedName = 'NCAAB';
+                break;
+            case 'NFL SUPER BOWL LV':
+                $convertedName = 'NFL';
+                break;
+            case 'ODDS TO WIN NCAA FOOTBALL CHAMPIONSHIP':
+                $convertedName = 'NCAAF';
+                break;
+        }
+        return $convertedName;
     }
     public function tabNames()
     {
